978f99b6e870cef7671af533e44114d4c0a257d8
Gabriel Maia <gabrielmaialva@gmail.com>
2020-10-16 15:46:08 -0300

    feat: create professionals migrations

---
+++ src/database/migrations/1602873098499-create_professionals.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class createProfessionals1602873098499
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'professionals',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar(50)',
+          },
+          {
+            name: 'email',
+            type: 'varchar(100)',
+          },
+          {
+            name: 'whatsapp',
+            type: 'varchar(11)',
+          },
+          {
+            name: 'knowledges',
+            type: 'varchar(50)[]',
+          },
+          {
+            name: 'bio',
+            type: 'text',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('professionals');
+  }
+}
